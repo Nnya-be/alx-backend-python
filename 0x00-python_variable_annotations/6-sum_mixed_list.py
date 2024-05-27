c98af4e85c9f77f48dfdb81787e363171a71e831
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-05-27 11:16:26 +0000

    Created: the complex types- mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Mixed integers and float lists sum."""
+
+
+def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+    """Calculate mixed numbers and output float."""
+    return (sum(mxd_lst))
