bb9a8986af2cd76bb0314dfba90ff7bfbcd074fb
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-05-27 11:12:47 +0000

    Created:complex types- list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Sum of list annotated function."""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Add all elements in a float list and returns a float."""
+    return sum(input_list)
