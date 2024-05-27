74cecd47050cf7ed3a7bad6ab3bfd1672915da0f
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-05-27 11:19:38 +0000

    Modified: to use the typing module

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Mixed integers and float lists sum."""
+from typing import Union
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+def sum_mixed_list(mxd_lst: list[Union[int | float]]) -> float:
     """Calculate mixed numbers and output float."""
     return (sum(mxd_lst))
