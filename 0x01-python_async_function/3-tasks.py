436a6de77c2d0287eab5846e38a189349adf9b4d
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-06-06 23:40:32 +0000

    Created:

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''Task 3's module.
+This module contains the function `task_wait_random` which creates
+an asynchronous task for the `wait_random` function.
+'''
+
+import asyncio
+from typing import Any
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''Creates an asynchronous task for wait_random.
+
+    Args:
+        max_delay (int): The maximum delay for the wait_random function.
+
+    Returns:
+        asyncio.Task: An asynchronous task for wait_random.
+    '''
+    return asyncio.create_task(wait_random(max_delay))
